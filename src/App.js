e0138c5ab8b8f87ac5badc0e774438bdb486ab19
Vassily Krasnov <mykrasnov@gmail.com>
2019-08-13 00:53:56 +0300

    Add inverse data flow

--- src/App.js
+++ src/App.js
@@ -75,6 +75,20 @@ class ProductTable extends React.Component {
 }
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+
   render() {
     const filterText = this.props.filterText;
     const inStockOnly = this.props.inStockOnly;
@@ -84,11 +98,15 @@ class SearchBar extends React.Component {
         <input
           type="text"
           placeholder="Search..."
-          value={filterText} />
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
         <p>
           <input
             type="checkbox"
-            checked={inStockOnly} />
+            checked={inStockOnly}
+            onChange={this.handleInStockChange}
+          />
           {' '}
           Only show products in stock
         </p>
@@ -104,6 +122,17 @@ class FilterableProductTable extends React.Component {
       filterText: '',
       inStockOnly: false,
     }
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({filterText});
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({inStockOnly});
   }
 
   render() {
@@ -111,7 +140,9 @@ class FilterableProductTable extends React.Component {
       <div>
         <SearchBar
           filterText={this.state.filterText}
-          inStockOnly={this.state.inStockOnly} />
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange} />
         <ProductTable
           products={this.props.products}
           filterText={this.state.filterText}
